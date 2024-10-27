991622c9af2dfce548a0f93432023e9f5f8a5d11
Tailon Galvao <tailontuuc@gmail.com>
2024-10-27 02:47:35 -0300

    Melhorias

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import numpy as np
 from st_on_hover_tabs import on_hover_tabs
 import importlib
 
-css_file_path = 'css/style.css'
+
+css_file_path = '../css/style.css'
 
 # Configurações da página
 st.set_page_config(page_title="Musicalizando", layout="wide")
@@ -12,7 +13,7 @@ st.set_page_config(page_title="Musicalizando", layout="wide")
 st.markdown('<style>' + open(css_file_path).read() + '</style>', unsafe_allow_html=True)
 
 # Sidebar com imagem e tabs de navegação
-image_path='/img/music.png'
+image_path='../img/music.png'
 with st.sidebar:
    try:
         st.image(
@@ -23,21 +24,21 @@ with st.sidebar:
    except Exception as e:
     st.error(f"Ocorreu um erro ao carregar a imagem: {e}")
     
-    selected = on_hover_tabs(
+   selected = on_hover_tabs(
         tabName=["Detector de Nota", 'Campo Harmônico', 'Baixar Video', 'Configurações'],
         iconName=['graphic_eq', 'music_note', 'browser_updated', 'browser_updated', 'settings'],
         default_choice=0
-    )
+   )
     
     # Rodapé no sidebar
-    st.markdown("---")
-    st.markdown(
+   st.markdown("---")
+   st.markdown(
         "<div style='position: fixed; bottom: 0; width: 100%; text-align: center;'>"
         "<p><strong>Criado por: Tailon Galvão</strong></p>"
         "<p>Pix do Café☕: (93)981009924</p>"
         "</div>",
         unsafe_allow_html=True
-    )
+   )
 
 # Redirecionamento para os arquivos de página
 if selected == "Detector de Nota":
