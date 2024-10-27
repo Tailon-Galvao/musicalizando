03ec3800de20826f3636540ccd445ea5f5a9a752
Tailon Galvao <tailontuuc@gmail.com>
2024-10-27 17:57:46 -0300

    Alterando

--- src/baixarVideo.py
+++ src/baixarVideo.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from pytube import YouTube
-from pytubefix import YouTube as TubefixYouTube  # Renomeando para evitar conflitos
 import os
 import time
 
@@ -15,25 +14,24 @@ def run():
     # Verifica se a URL é válida
     if url:
         try:
-            yt = TubefixYouTube(url, use_po_token=True)
+            yt = YouTube(url)
 
             if yt:
                 st.video(url)  # Exibe uma prévia do vídeo
                 
-                st.spinner("Processando download...")
-
-                # Define o formato de download
-                if download_option == "Vídeo":              
-                    video_stream = yt.streams.get_highest_resolution()
-                    video_file = video_stream.download()
-                    file_name = os.path.basename(video_file)
-                    mime_type = "video/mp4"
-                
-                else:
-                    video_stream = yt.streams.get_audio_only()
-                    video_file = video_stream.download()
-                    file_name = os.path.basename(video_file)
-                    mime_type = "audio/mp3"
+                with st.spinner("Processando download..."):
+                    # Define o formato de download
+                    if download_option == "Vídeo":              
+                        video_stream = yt.streams.get_highest_resolution()
+                        video_file = video_stream.download()
+                        file_name = os.path.basename(video_file)
+                        mime_type = "video/mp4"
+                    
+                    else:
+                        video_stream = yt.streams.get_audio_only()
+                        video_file = video_stream.download()
+                        file_name = os.path.basename(video_file)
+                        mime_type = "audio/mp3"
 
                 # Fornece um link para download
                 with open(video_file, "rb") as f:
@@ -45,7 +43,9 @@ def run():
                     )
                     
                 st.success(f"{download_option} baixado com sucesso!")
-                time.sleep(5)
+                time.sleep(5)  # Atraso para permitir que o usuário veja a mensagem
+
+                # Remove o arquivo após o download
                 if os.path.exists(video_file):
                     os.remove(video_file)
 
